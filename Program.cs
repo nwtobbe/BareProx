76135a3f238467f8eaaa925fe3fd7f4817137e6b
nwtobbe <tobias.modig@bareapp.se>
2025-05-31 18:34:35 +0200

    update

--- Program.cs
+++ Program.cs
@@ -237,9 +237,9 @@ builder.Services.AddRazorPages();
 
 
 
-#if DEBUG
-builder.Logging.SetMinimumLevel(LogLevel.Debug);
-#else
+//#if DEBUG
+//builder.Logging.SetMinimumLevel(LogLevel.Debug);
+//#else
 //---Lock down-- - Fix _LoginPartial.cshtml to enable lockout and uncomment this section
 if (isConfigured)
 {
@@ -252,7 +252,7 @@ if (isConfigured)
     });
 }
 builder.Logging.SetMinimumLevel(LogLevel.Warning);
-#endif
+// #endif
 
 // 4) Configure Kestrel to use the SelfSignedCertificateService for HTTPS
 builder.WebHost.ConfigureKestrel(options =>
